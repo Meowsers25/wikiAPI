95a1b125d1e13fcc2e162f4e7271931d565c1190
Chris <c.lamothe25@gmail.com>
2017-08-11 15:37:09 -0400

    added jquery to give results

--- script.js
+++ script.js
@@ -9,7 +9,19 @@ $(document).ready(function() {
             type: "GET",
             dataType: "json",
             success: function(response){
-                console.log(response);
+                // console.log(response[1][0]);
+                // console.log(response[2][0]);
+                // console.log(response[3][0]);
+                // $("#results").html(response[1][0]);
+                var keyWord = response[1];
+                var description = response[2];
+                var html = "<ul class='list-group'style='list-style: none;'>";
+                $.each(keyWord, function (index, keyWord) {
+                    html += "<li class='list-item'>" + "<em>" + keyWord + "</em>" + "<br>" + description + "</li>";
+                });
+                html += "</ul>";
+                $("#results").html(html);
+
             },
             error: function(error){
                 console.log('error');
