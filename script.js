83aa6acf4bf213414a95d5685ad846581f2c95d2
Chris <c.lamothe25@gmail.com>
2017-08-12 10:31:15 -0400

    modified script.js

--- script.js
+++ script.js
@@ -7,20 +7,27 @@ $(document).ready(function() {
         $.ajax({
             url: url,
             type: "GET",
-            dataType: "json",
+            dataType: "json", //needed to add this
             success: function(response){
+                console.log(response[1].length);
                 // console.log(response[1][0]);
                 // console.log(response[2][0]);
                 // console.log(response[3][0]);
                 // $("#results").html(response[1][0]);
-                var keyWord = response[1];
+                var keyWords = response[1];
                 var description = response[2];
+                var anchor = response[3];
                 var html = "<ul class='list-group'style='list-style: none;'>";
-                $.each(keyWord, function (index, keyWord) {
-                    html += "<li class='list-item'>" + "<em>" + keyWord + "</em>" + "<br>" + description + "</li>";
+                $.each(response[1], function (i, element) {
+                    console.log(keyWords[i]);
+                    console.log(description[i]);
+                    console.log(anchor[i]);
+                    html += "<li class='list-item'><a style='text-decoration:none; color:inherit;' href=" + anchor[i] + ">" + "<em>" + keyWords[i] + "</em>" + "</a>" + "<br>" + description[i] + "</li>";
                 });
                 html += "</ul>";
+                
                 $("#results").html(html);
+                $("#search").val('');
 
             },
             error: function(error){
